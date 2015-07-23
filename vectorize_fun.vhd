a6827b3c569a9f8a1c29c437b54926cba6f9ceed
Andres Demski <andresdemski@gmail.com>
2015-07-22 22:54:50 -0300

    Se agregan delayline y mac con preadder. No fueron probados. (igual no creo que esten mal, yo no tengo errores ajajaj)

--- vectorize_fun.vhd
+++ vectorize_fun.vhd
@@ -3,6 +3,7 @@ use ieee.std_logic_1164.all;
 
 package extra_functions is
 	function vectorize(s : std_logic) return std_logic_vector;
+	function log2 (x : positive) return natural;
 end package extra_functions;
 
 package body extra_functions is
@@ -12,5 +13,15 @@ package body extra_functions is
 		v(0) := s;
 		return v;
 	end function;
+	
+	function log2 (x : positive) return natural is
+      variable i : natural;
+   begin
+      i := 0;  
+      while (2**i < x) loop
+         i := i + 1;
+      end loop;
+      return i;
+   end function;
 
 end package body extra_functions;
