647f13483fd96faec6bf006be10dad188483d9c2
ivpa93 <ivan-paunovic@hotmail.com>
2015-07-26 16:33:43 -0300

    Agregado dos generics para extender en signo y que reconozca el bloque mac con preadder dentro de un dsp. Los F-F de entrada se los puede llegar a sacar (igual los implementa dentro del dsp).

--- preadd_mac/preadd_mac.vhd
+++ preadd_mac/preadd_mac.vhd
@@ -4,14 +4,16 @@ use ieee.numeric_std.all;
 
 entity preadd_mac is
 	generic(
-			N : integer := 17; -- SE DEBE ENTRAR COMO CA2!!!
-			N_OUT : integer := 48
+			N_in_pre: natural:=16;--cantidad de bits que llegan al pre-adder
+			N_in_mul: natural:=16;--cantidad de bits llegan a multiplicador
+			N : integer := 18; --Cantidad de bits de las entradas reales dsp48A1
+			N_OUT : integer := 48 --Salida acumulador dsp48A1
 	);
 	
 	port (
-		adder_input1 : in std_logic_vector(N-1 downto 0);
-		adder_input2 : in std_logic_vector(N-1 downto 0);
-		coef_input : in std_logic_vector(N downto 0);
+		adder_input1 : in std_logic_vector(N_in_pre-1 downto 0);
+		adder_input2 : in std_logic_vector(N_in_pre-1 downto 0);
+		coef_input : in std_logic_vector(N_in_mul-1 downto 0);
 		
 		output : out std_logic_vector (N_OUT -1 downto 0);
 		
@@ -22,12 +24,12 @@ entity preadd_mac is
 end entity preadd_mac;
 
 architecture RTL of preadd_mac is
-	signal adder_input1_resized : std_logic_vector(N downto 0):=std_logic_vector(to_signed(0,N+1));
-	signal adder_input2_resized : std_logic_vector(N downto 0):=std_logic_vector(to_signed(0,N+1));
-	signal pre: std_logic_vector(N downto 0):=std_logic_vector(to_signed(0,N+1));
-	signal mul: std_logic_vector(2*N+1 downto 0):=std_logic_vector(to_signed(0,2*N+2));
-	signal coef_input_i: std_logic_vector(N downto 0):=std_logic_vector(to_signed(0,N+1));
-	signal coef_input_ii: std_logic_vector(N downto 0):=std_logic_vector(to_signed(0,N+1));
+	signal adder_input1_resized : std_logic_vector(N-1 downto 0):=std_logic_vector(to_signed(0,N));
+	signal adder_input2_resized : std_logic_vector(N-1 downto 0):=std_logic_vector(to_signed(0,N));
+	signal pre: std_logic_vector(N-1 downto 0):=std_logic_vector(to_signed(0,N));
+	signal mul: std_logic_vector(2*N-1 downto 0):=std_logic_vector(to_signed(0,2*N));
+	signal coef_input_i: std_logic_vector(N-1 downto 0):=std_logic_vector(to_signed(0,N));
+	signal coef_input_ii: std_logic_vector(N-1 downto 0):=std_logic_vector(to_signed(0,N));
 	
 	signal acc: std_logic_vector(N_OUT-1 downto 0):=std_logic_vector(to_signed(0,N_OUT));
 	
@@ -44,9 +46,16 @@ begin
 				coef_input_i <= (others => '0');
 				coef_input_ii <= (others => '0');
 			elsif ce = '1' then
-				adder_input1_resized <= adder_input1(N-1) & adder_input1;
-				adder_input2_resized <= adder_input2(N-1) & adder_input2;
-				coef_input_i <= coef_input;
+				--extensiones de signo
+				adder_input1_resized(N-1 downto N_in_pre) <= (others=>adder_input1(N_in_pre-1));
+				adder_input1_resized(N_in_pre-1 downto 0) <= adder_input1(N_in_pre-1 downto 0);
+				
+				adder_input2_resized(N-1 downto N_in_pre) <= (others=>adder_input2(N_in_pre-1));
+				adder_input2_resized(N_in_pre-1 downto 0) <= adder_input2(N_in_pre-1 downto 0);
+				
+				coef_input_i(N-1 downto N_in_mul) <= (others=>coef_input(N_in_mul-1));				
+				coef_input_i(N_in_mul-1 downto 0) <= coef_input(N_in_mul-1 downto 0);
+				
 				coef_input_ii <= coef_input_i;
 			end if;
 		end if;
@@ -55,9 +64,6 @@ begin
 	make_operations: process (clk)
 	begin
 		if rising_edge(clk) then
---			pre <= pre;
---			mul <= mul;
---			acc <= acc;
 			if rst = '1' then
 				pre<= (others => '0');
 				mul <= (others => '0');
