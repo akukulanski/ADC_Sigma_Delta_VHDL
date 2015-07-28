b808818d386b1f4b07b6310ff8b3919a80a64e1e
kuku22 <kuku2206@gmail.com>
2015-07-27 23:57:56 -0300

    decimador sin retraso. renombro Coefficients.vhd por my_coeffs.vhd

--- decimator/decimator.vhd
+++ decimator/decimator.vhd
@@ -19,24 +19,21 @@ end entity decimator;
 
 architecture RTL of decimator is
 	constant B      : natural := log2(R); -- se redondea log2(R) para arriba
-	signal count    : unsigned(B - 1 downto 0); --counter
-	signal ce_out_i : std_logic;
+	signal count    : unsigned(B - 1 downto 0) := (others=>'1'); --counter
+
 begin
-	ce_out <= ce_out_i;
+		
+	ce_out <= ce_in when (count=to_unsigned(0,B) or B=0) else
+		'0';
 
 	ce_decimate : process(clk) is
 	begin
 		if rising_edge(clk) then
 			if rst = '1' then
-				count    <= (others => '0');
-				ce_out_i <= '0';
+				count    <= (others => '1');
 			else
-				ce_out_i <= '0';
 				if ce_in = '1' then
 					count <= count + to_unsigned(1, B);
-					if count = (to_unsigned(R - 1, B)) then
-						ce_out_i <= '1';
-					end if;
 				end if;
 			end if;
 		end if;
