26dafa846d5693958206b4a8c85b5caa9476855a
ivpa93 <ivan-paunovic@hotmail.com>
2015-12-07 17:04:32 -0300

    Corregido error formula (18)

--- extras/scripts_matlab/SQNR_pasivo_ord1.m
+++ extras/scripts_matlab/SQNR_pasivo_ord1.m
@@ -5,23 +5,38 @@ wp=2*pi*fp;
 Trc=1/wp;%constante de tiempo filtro
 %Trc=10;
 Ts=1/fs;
+
+% pasivo ord 1
 H=@(f) 1/2./(1+1j*2*pi*Trc*f);
+% activo ord 1
 %H=@(f) 1./(1i*2*pi*Trc*f);
+
+% aprox teorica ganancia
 %G=abs(1/feval(H,fs/2));
+% ganancia simulación a 1kHz
 G=1650;
-%G=3.3/3e-3;
-Heq=@(f) 1/2 * (1-exp(-Ts/Trc))./(1-exp(-Ts/Trc)*exp(-1i*2*pi*f*Ts));
-%Heq=@(f) 1/2 * (1-exp(-Ts/Trc))./(exp(1i*2*pi*f*Ts)-exp(-Ts/Trc));
-%Heq=@(f) exp(-1i*2*pi*f*Ts)./(1-exp(-1i*2*pi*f*Ts));
+
+% Filtro de lazo equivalente pasivo orden 1
+Heq=@(f) 1/2 * exp(-1i*2*pi*f*Ts)* (1-exp(-Ts/Trc))./(1-exp(-Ts/Trc)*exp(-1i*2*pi*f*Ts));
+
+
 Fs=0:2000:fs;
 Fp=0:1:11000;
 Fa=fs-11000:1:fs+11000;
 
-%figure(5);
-%plot(Fp,20*log10( abs( feval(Heq,Fp) ) ) );
+% Transferencia ruido pasivo orden 1
+Hn=@(f) 1./(1+G*Heq(f));
+% lo mismo despejado a mano para verificar
+%Hn=@(f) 2 *(1-exp(-Ts/Trc)*exp(-1i*2*pi*f*Ts))./(2+exp(-1i*2*pi*f*Ts)*(G-(G+2)*exp(-Ts/Trc)));
+% La transferencia de ruido activo ord 1
+%Hn=@(f) 1-exp(-1i*2*pi*f*Ts);
 
-Hn=@(f) 1./(1+G*feval(Heq,f));
-Hx=@(f) G*feval(H,f).*feval(Hn,f);
+% Transf señal pasivo ord 1
+Hx=@(f) G.*feval(H,f).*feval(Hn,f);
+% para activo ord 1
+%Hx=@(f) (1-exp(-1i*2*pi*f*Ts))./(1i*2*pi*f*Ts);
+
+%% Ploteo
 figure(1);
 subplot(2,2,[1,2]);
 plot(Fp/1000,20/log(10)*log( abs( feval(Hx,Fp) ) ) );
@@ -34,16 +49,19 @@ xlabel('frec[kHz]','FontSize',16);%,'FontWeight','bold');
 ylabel('H_N[dB]','FontSize',16);%'FontWeight','bold');
 set(gca,'fontsize',16);
 subplot(224);
-plot(Fa/1e6,20/log(10)*log( abs( feval(Hx,Fa) ) ) );
+plot(Fa/1e6,20/log(10)*log( abs( feval(Hx,Fa) )/max(abs(feval(Hx,Fp))) ) );
 xlabel('frec[MHz]','FontSize',16);%,'FontWeight','bold');
 ylabel('H_X[dB]','FontSize',16);%,'FontWeight','bold');
 set(gca,'fontsize',16);
+
 %figure(2);
 %plot(Fp,20/log(10)*log( abs( feval(Hn,Fp) ) ) );
 %figure(3);
 %plot(Fs,20/log(10)*log( abs( feval(Hx,Fs) ) ) );
 %figure(4);
 %plot(Fs,20/log(10)*log( abs( feval(Hn,Fs) ) ) );
+
+%% Calculo ENOB
 
 int= integral(@(f) abs(feval(Hn,f)).^2,0,fp);
 int
